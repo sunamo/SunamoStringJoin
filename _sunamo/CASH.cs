769fe9d319a05c544e784af4a6df7a189b9ee95a
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-04 12:09:15 +0100

    .

--- _sunamo/CASH.cs
+++ _sunamo/CASH.cs
@@ -6,20 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SunamoStringJoin._sunamo
+namespace SunamoStringJoin._sunamo;
+
+internal class CASH
 {
-    internal class CASH
+    internal static List<string> WrapWithIfFunc(Func<string, string, bool, bool> f, bool invert, string mustContains, string wrapWith, params string[] whereIsUsed2)
     {
-        internal static List<string> WrapWithIfFunc(Func<string, string, bool, bool> f, bool invert, string mustContains, string wrapWith, params string[] whereIsUsed2)
+        for (int i = 0; i < whereIsUsed2.Length; i++)
         {
-            for (int i = 0; i < whereIsUsed2.Length; i++)
+            if (f.Invoke(whereIsUsed2[i], mustContains, invert))
             {
-                if (f.Invoke(whereIsUsed2[i], mustContains, invert))
-                {
-                    whereIsUsed2[i] = wrapWith + whereIsUsed2[i] + wrapWith;
-                }
+                whereIsUsed2[i] = wrapWith + whereIsUsed2[i] + wrapWith;
             }
-            return whereIsUsed2.ToList();
         }
+        return whereIsUsed2.ToList();
     }
 }
