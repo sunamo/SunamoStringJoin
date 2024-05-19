902faaf111a4593a8033e16a0463c1bf018f6b4d
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-19 14:30:44 +0200

    Removed empty from _sunamo

--- _sunamo/CA.cs
+++
@@ -1,7 +0,0 @@
-namespace SunamoStringJoin;
-
-//namespace SunamoStringJoin._sunamo;
-//internal class CA
-//{
-//    internal static Func<Func<string, string, bool, bool>, bool, string, string, string[], string[]> WrapWithIfFunc;
-//}
--- _sunamo/SH.cs
+++
@@ -1,7 +0,0 @@
-namespace SunamoStringJoin;
-
-//namespace SunamoStringJoin._sunamo;
-//internal class SH
-//{
-//    internal static Func<string, string, string[], string> ReplaceAll;
-//}
