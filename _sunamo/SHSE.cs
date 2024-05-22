db637dd7b909c801f2e06558fe2b49fa56858abb
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-22 07:56:02 +0200

    Public to internal in _sunamo

--- _sunamo/SHSE.cs
+++ _sunamo/SHSE.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SunamoStringJoin;
 internal class SHSE
 {
-    public static string TrimEnd(string name, string ext)
+    internal static string TrimEnd(string name, string ext)
     {
         while (name.EndsWith(ext)) return name.Substring(0, name.Length - ext.Length);
 
