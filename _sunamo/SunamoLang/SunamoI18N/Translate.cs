87bc52634c626a736e8c97acc8415acf2848a868
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-25 11:52:27 +0100

    Renamed file

--- _sunamo/SunamoLang/SunamoI18N/Translate.cs
+++ _sunamo/SunamoLang/SunamoI18N/Translate.cs
@@ -1,6 +1,6 @@
 namespace SunamoStringJoin._sunamo.SunamoLang.SunamoI18N;
 
-internal class sess
+internal class Translate
 {
     internal static string? FromKey(string probablyWasCalledWithSwithechDelimiterAndParts)
     {
